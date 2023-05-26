605cc2f94c6a2e44876411b97afc254c3f83593c
Masih <masih.saldanha@gmail.com>
2023-05-25 21:49:33 -0300

    chore: last adjustments

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,9 +4,9 @@ import { useNavigate } from "react-router-dom";
 function ErrorPage() {
   const navigate = useNavigate();
 
-  useEffect(() => navigate("/signup"), []);
+  useEffect(() => navigate("/signin"), []);
 
-  return <h2>This page doesn't exist, redirecting you to the sign up page</h2>;
+  return <h2 className="text-center">This page doesn't exist, redirecting you to the sign up page</h2>;
 }
 
 export default ErrorPage;
